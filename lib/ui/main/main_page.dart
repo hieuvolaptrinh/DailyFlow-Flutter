721f6a295a418325a06b9dcdbc0a3c2ddab73bc7
hieuvolaptrinh <vndhieuak@gmail.com>
2025-11-03 23:41:22 +0700

    install shared preferences

--- lib/ui/main/main_page.dart
+++ lib/ui/main/main_page.dart
@@ -8,13 +8,41 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  List<Widget> pages = [];
+
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pages = [
+      Container(color: Colors.red),
+      Container(color: Colors.green),
+      Container(),
+      Container(color: Colors.blue),
+      Container(color: Colors.yellow),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF121212),
-      body: Container(),
+      body: pages.elementAt(_currentPage),
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color(0xFF1E1E1E),
+        backgroundColor: const Color(0xFF121212),
+        unselectedItemColor: Colors.white,
+        selectedItemColor: const Color(0XFF8687E7),
+        type: BottomNavigationBarType.fixed,
+        currentIndex:
+            _currentPage, // index hiện tại của thằng BottomNavigationBar
+        onTap: (index) {
+          if (index != 2) {
+            setState(() {
+              _currentPage = index;
+            });
+          }
+        },
         items: [
           BottomNavigationBarItem(
             icon: Image.asset(
@@ -23,24 +51,47 @@ class _MainPageState extends State<MainPage> {
               height: 24,
               fit: BoxFit.fill,
             ),
+            activeIcon: Image.asset(
+              "assets/images/home-2.png",
+              width: 24,
+              height: 24,
+              fit: BoxFit.fill,
+              color: const Color(0XFF8687E7),
+            ),
             label: "Index",
           ),
 
           BottomNavigationBarItem(
+            backgroundColor: const Color(0xFF121212),
             icon: Image.asset(
               "assets/images/calendar.png",
               width: 24,
               height: 24,
               fit: BoxFit.fill,
             ),
+            activeIcon: Image.asset(
+              "assets/images/calendar.png",
+              width: 24,
+              height: 24,
+              fit: BoxFit.fill,
+              color: const Color(0XFF8687E7),
+            ),
             label: "Calendar",
           ),
+          BottomNavigationBarItem(icon: Container(), label: ""),
           BottomNavigationBarItem(
             icon: Image.asset(
               "assets/images/clock.png",
               width: 24,
               height: 24,
               fit: BoxFit.fill,
+            ),
+            activeIcon: Image.asset(
+              "assets/images/clock.png",
+              width: 24,
+              height: 24,
+              fit: BoxFit.fill,
+              color: const Color(0XFF8687E7),
             ),
             label: "Focus",
           ),
@@ -51,10 +102,35 @@ class _MainPageState extends State<MainPage> {
               height: 24,
               fit: BoxFit.fill,
             ),
+            activeIcon: Image.asset(
+              "assets/images/user.png",
+              width: 24,
+              height: 24,
+              fit: BoxFit.fill,
+              color: const Color(0XFF8687E7),
+            ),
             label: "Profile",
           ),
         ],
       ),
+      floatingActionButton: Container(
+        width: 64,
+        height: 64,
+        // decoration dùng để trang trí cho container
+        decoration: BoxDecoration(
+          color: Color(0XFF8687E7),
+          borderRadius: BorderRadius.circular(32),
+        ),
+        child: IconButton(
+          onPressed: () {
+            setState(() {
+              _currentPage = 2;
+            });
+          },
+          icon: Icon(Icons.add, size: 30, color: Colors.white),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
